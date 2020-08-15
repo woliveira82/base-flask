1992c96146158ad1be7f3d9acd1a95941a8ea30b
woliveira82 <woliveira82@gmail.com>
2020-08-15 13:37:06 -0300

    Updated config file, added more config options

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
+import os.path
+
+
+def sqlite_path():
+    sqlite_path = os.path.dirname(os.path.abspath(__file__))
+    return sqlite_path
+
+ENV = 'production'
 DEBUG = False
 SQLALCHEMY_ECHO = False
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{sqlite_path()}storage.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
