c24f7547fab216d297f6dd671dd83d361f3628f6
woliveira82 <woliveira82@gmail.com>
2020-08-15 13:46:46 -0300

    Updated database to mysql

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
-import os.path
-
-
-def sqlite_path():
-    sqlite_path = os.path.dirname(os.path.abspath(__file__))
-    return sqlite_path
-
 ENV = 'production'
 DEBUG = False
 SQLALCHEMY_ECHO = False
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{sqlite_path()}storage.db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/db_name'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
