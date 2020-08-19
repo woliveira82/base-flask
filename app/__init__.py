9121347abd1ddde9ab34e70165b2d3550fb954fb
woliveira82 <woliveira82@gmail.com>
2020-08-19 19:25:35 -0300

    Added error handler

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,32 @@ import importlib
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app.inc import ResponseException
+from werkzeug.exceptions import HTTPException
+
+
+def handler_error(app):
+    
+
+    @app.errorhandler(HTTPException)
+    def handler_error(error):
+        return ResponseException(error.name, status=error.code).to_dict(), error.code
+
+
+    @app.errorhandler(ResponseException)
+    def handler_error(error):
+        response = error.to_dict()
+        return response, response['status']
+
+
+    @app.errorhandler(Exception)
+    def handler_error(error):
+        data = str(error) if app.config['DEBUG'] else None
+        return ResponseException(data, status=500).to_dict(), 500
 
 app = Flask(__name__, instance_relative_config=True)
+handler_error(app)
 app.config.from_pyfile('config.py')
-
 db = SQLAlchemy(app)
 
 modules = [
